6627e5e38b37f982d2cdd119e5b8cbf2aca24322
Salvist <richiechandra47@yahoo.com>
2025-06-03 23:32:49 -0400

    whole page background color to white

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,6 @@ import JoinOurCommunitySection from "@/components/join_our_community_section";
 import MainSection from "@/components/main_section";
 import SundayServicesSection from "@/components/sunday_services_section";
 import WeeklyServices from "@/components/weekly_services";
-import {
-  ClockIcon,
-  MapIcon,
-  MapPinIcon,
-  MegaphoneIcon,
-} from "@heroicons/react/24/outline";
 
 export default function Home() {
   return (
