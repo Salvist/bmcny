8a4fbcc3c5aa1bca5850bd270f8a6b91fa5f660b
Salvist <richiechandra47@yahoo.com>
2025-06-04 21:37:25 -0400

    add location

--- src/components/main_section.tsx
+++ src/components/main_section.tsx
@@ -1,10 +1,11 @@
+import { MapPinIcon } from "@heroicons/react/24/outline";
 import FacebookIcon from "./icons/facebook_icon";
 import InstagramIcon from "./icons/instagram_icon";
 import YoutubeIcon from "./icons/youtube_icon";
 
 export default function MainSection() {
   return (
-    <section className="relative bg-amber-50 px-4 py-32 mt-16 text-black text-center flex items-center justify-center overflow-hidden">
+    <section className="relative bg-amber-50 px-4 py-24 mt-16 text-black text-center flex items-center justify-center overflow-hidden">
       <div className="absolute inset-0 bg-[url('/cross_on_top.webp')] bg-cover bg-bottom" />
       <div className="absolute inset-0 bg-amber-50/70" />
       <div className="relative z-10 w-full flex flex-col items-center justify-center">
@@ -14,10 +15,17 @@ export default function MainSection() {
         <p className="mt-8 text-lg">
           An Indonesian community church filled with loving members of Christ.
         </p>
-        <button className="bg-yellow-400 text-white px-6 py-2 rounded-lg font-bold mt-8">
-          Join us!
-        </button>
-        <div className="mt-8 flex gap-6">
+        <div className="flex gap-4">
+          <button className="bg-yellow-400 text-orange-700 px-6 py-2 rounded-lg font-bold mt-8">
+            Join Us This Sunday!
+          </button>
+          <button className="outline outline-orange-700 text-orange-700 px-6 py-2 rounded-lg font-bold mt-8">
+            Watch Online
+          </button>
+        </div>
+
+        {/* Social Media Links */}
+        <div className="mt-12 flex gap-6">
           <a
             href="https://www.instagram.com/bmc_ny/"
             target="_blank"
@@ -49,6 +57,17 @@ export default function MainSection() {
             </div>
           </a>
         </div>
+        <a
+          href="https://maps.app.goo.gl/JWKGhHKz9nJELbUK9"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="mt-4"
+        >
+          <div className="flex gap-1 underline">
+            <MapPinIcon className="size-6" />
+            <span>92-14 63rd Dr, Rego Park, NY 11374</span>
+          </div>
+        </a>
       </div>
     </section>
   );
