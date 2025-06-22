faf226803a8457cc456a3cb57d61570289074dd4
Salvist <richiechandra47@yahoo.com>
2025-06-22 16:00:13 -0400

    shorter about church description

--- src/components/about_us_section.tsx
+++ src/components/about_us_section.tsx
@@ -11,21 +11,16 @@ export default function AboutUsSection() {
             Bethany Miracle Center of New York
           </span>
           <p className="mt-4">
-            Bethany Miracle Center of New York (BMC NY) is an Indonesian
-            Christian church with a heart for building faith and spreading the
-            Gospel. Located in the vibrant city of New York, our church offers a
-            family-oriented atmosphere where believers can grow together in
-            Christ. We gather weekly for Sunday services conducted in
-            Indonesian, with English translation available upon request,
-            ensuring everyone feels welcomed and included. In addition to our
-            main service, we provide a dedicated kids ministry to help young
-            hearts grow in their faith. Whether you're new to the city, seeking
-            spiritual growth, or looking for a Christ-centered community, BMC NY
-            invites you to worship, connect, and walk in faith with us.
+            An Indonesian Christian church in NYC, passionate about building
+            faith and sharing the Gospel. With a warm, family-oriented
+            atmosphere, we offer Indonesian Sunday services with English
+            translation available, plus a dedicated kids ministry. Whether
+            you're new to the city or seeking a Christ-centered community,
+            you're welcome to grow and worship with us.
           </p>
         </div>
         <h3 className="mt-4 text-2xl font-bold font-montserrat">Vision</h3>
-        <p className="mt-2">
+        <p>
           To be a church that is known for its love, compassion, and
           transformational impact in the community.
         </p>
@@ -38,3 +33,18 @@ export default function AboutUsSection() {
     </section>
   );
 }
+
+/*
+OLD About Church
+Bethany Miracle Center of New York (BMC NY) is an Indonesian
+Christian church with a heart for building faith and spreading the
+Gospel. Located in the vibrant city of New York, our church offers a
+family-oriented atmosphere where believers can grow together in
+Christ. We gather weekly for Sunday services conducted in
+Indonesian, with English translation available upon request,
+ensuring everyone feels welcomed and included. In addition to our
+main service, we provide a dedicated kids ministry to help young
+hearts grow in their faith. Whether you're new to the city, seeking
+spiritual growth, or looking for a Christ-centered community, BMC NY
+invites you to worship, connect, and walk in faith with us.
+*/
